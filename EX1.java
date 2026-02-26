f12432868cfe43f59a53031c088793ab0fcd750e
joaogabrielmg <joao.biel.srs@gmail.com>
2026-02-25 21:38:40 -0300

    exercicio 1

---
+++ EX1.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main() {
+
+        /*String nome = "Joao";
+        //idade="30";//TEXTO
+        System.out.println(nome);
+        System.out.println(nome.length());
+        System.out.println(nome.toUpperCase());
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.getBytes());
+        System.out.println(nome.replace("Joao", "Gabriel"));*/
+    }
+}
